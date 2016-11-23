1e57e019112c8c105c5057507213d8f4b7b2fae3
ivan sioson <ivan.sioson@kitandace.com>
2016-11-23 11:39:46 -0800

    Master

---
+++ TST_NS2.0/test20_clientscript.js
@@ -0,0 +1,32 @@
+/**
+ *@NApiVersion 2.x
+ *@NScriptType ClientScript
+ *@NScriptName client_script_demo
+ */
+define(['N/record'],
+    function(record) {
+        function alertPageLoaded(context) {
+            alert('Page is loaded');
+        };
+        function fieldChanged(context) {
+            var currentRecord = context.currentRecord;
+            auditRecordChanges(currentRecord.type, currentRecord.id);
+            updateMemoFieldWhenItemFieldIsChanged(context);
+        }
+        function auditRecordChanges(type, id) {
+            /*var logRecord = record.create({type: 'customrecord_log', isDynamic: true});
+            logRecord.setValue('custrecord_recordType', type);
+            logRecord.setValue('custrecord_recordId', id);
+            logRecord.setValue('custrecord_changedDate', new Date());
+            logRecord.save();*/
+        }
+        function updateMemoFieldWhenItemFieldIsChanged(context) {
+            if(context.fieldId === 'item') {
+                context.currentRecord.setValue('memo', 'item field is changed');
+            }
+        }
+        return({
+            pageInit: alertPageLoaded,
+            fieldChanged: fieldChanged
+        });
+});
